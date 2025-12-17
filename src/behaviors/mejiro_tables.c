cc82c4bbaee05a9cd9bea5f2048732bf11d41ca1
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-17 21:49:51 +0900

    Add files via upload

---
+++ src/behaviors/mejiro_tables.c
@@ -0,0 +1,44 @@
+#include "mejiro_tables.h"
+
+const struct mejiro_kv mj_commands[] = {
+    {"-SKNA", "{^\\{\\}^}{#Left}{^}"},
+    {"-STYI", "{^<>^}{#Left}{^}"},
+    {"-KNA", "{^[]^}{#Left}{^}"},
+    {"-SKN", "{^\\}^}"},
+    {"-SNA", "{^\\{^}"},
+    {"-STY", "{^>^}"},
+    {"-SYI", "{^<^}"},
+    {"-TYI", "{^()^}{#Left}{^}"},
+    {"-AU", "{#BackSpace}"},
+    {"-IU", "{#Delete}"},
+    {"-KN", "{^]^}"},
+    {"-KY", "{^%^}"},
+    {"-NA", "{^[^}"},
+    {"-TN", "{^/^}"},
+    {"-TY", "{^)^}"},
+    {"-YI", "{^(^}"},
+    {"tk#", "{PLOVER:TOGGLE}"},
+    {"#k", "{#F13}"},
+    {"#t", "{#F14}"},
+    {"-A", "{^}{#Left}{^}"},
+    {"-I", "{^}{#Home}{^}"},
+    {"-K", "{^}{#Right}{^}"},
+    {"-N", "{^}{#Down}{^}"},
+    {"-S", "{#Escape}"},
+    {"-T", "{^}{#End}{^}"},
+    {"-U", "=undo"},
+    {"-Y", "{^}{#Up}{^}"},
+    {"t#", "{PLOVER:SWITCH_SYSTEM:English Stenotype}{#F13}{^}"},
+    {"#", "=repeat_last_translation"}
+};
+const size_t mj_commands_len = sizeof(mj_commands) / sizeof(mj_commands[0]);
+
+/* Placeholders: keep linker happy even if core references them. */
+const struct mejiro_kv mj_users[] = { };
+const size_t mj_users_len = 0;
+
+const struct mejiro_kv mj_abstract[] = { };
+const size_t mj_abstract_len = 0;
+
+const struct mejiro_kv mj_verbs[] = { };
+const size_t mj_verbs_len = 0;
