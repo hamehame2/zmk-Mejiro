3e62ebcc0aacf606af1044c77df0cc3b29151dba
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-17 22:47:21 +0900

    Update mejiro_core.h

--- include/mejiro/mejiro_core.h
+++ include/mejiro/mejiro_core.h
@@ -1,3 +1,15 @@
 #pragma once
-/* Back-compat wrapper: allow '#include "mejiro/mejiro_core.h"' */
-#include "../mejiro_core.h"
+
+#include <stdbool.h>
+#include <zmk_naginata/nglistarray.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Called by the Naginata hook when a chord is finalized. */
+bool mejiro_try_emit_from_nginput(const NGListArray *nginput);
+
+#ifdef __cplusplus
+}
+#endif
