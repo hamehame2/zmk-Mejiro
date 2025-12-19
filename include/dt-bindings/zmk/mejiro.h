f6b2d4dbf85249d5301fcb9dbba3a9cf17b0d21c
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-19 20:57:57 +0900

    Add files via upload

---
+++ include/dt-bindings/zmk/mejiro.h
@@ -0,0 +1,29 @@
+#pragma once
+
+// 必須
+#define MJ_H   1
+#define MJ_X   2
+
+// メジロ論理キー（飛び地・欠番OK）
+#define MJ_TN  10
+#define MJ_TR  11
+#define MJ_YL  20
+#define MJ_YR  21
+#define MJ_IL  30
+#define MJ_IR  31
+#define MJ_UL  40
+#define MJ_UR  41
+#define MJ_SL  50
+#define MJ_SR  51
+#define MJ_KL  60
+#define MJ_KR  61
+#define MJ_NL  70
+#define MJ_NR  71
+#define MJ_AL  80
+#define MJ_AR  81
+#define MJ_nL  90
+#define MJ_nR  91
+#define MJ_tL  100
+#define MJ_tR  101
+#define MJ_kL  110
+#define MJ_kR  111
