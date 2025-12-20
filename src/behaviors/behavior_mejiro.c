b585a444668372d22836184ea596f8c7a78f5402
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-20 13:11:18 +0900

    Update behavior_mejiro.c

--- src/behaviors/behavior_mejiro.c
+++ src/behaviors/behavior_mejiro.c
@@ -1,7 +1,3 @@
-/*
- * Minimal Mejiro behavior: just logs param on press/release.
- */
-
 #define DT_DRV_COMPAT zmk_behavior_mejiro
 
 #include <zephyr/device.h>
@@ -11,12 +7,11 @@
 #include <drivers/behavior.h>
 #include <zmk/behavior.h>
 
-LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
+LOG_MODULE_REGISTER(behavior_mejiro, CONFIG_ZMK_LOG_LEVEL);
 
 static int mejiro_pressed(struct zmk_behavior_binding *binding,
                           struct zmk_behavior_binding_event event) {
     ARG_UNUSED(event);
-    /* binding->param1 が keymap の &mj <param> の param */
     LOG_INF("MEJIRO press: param=%d", binding->param1);
     return 0;
 }
@@ -38,15 +33,10 @@ static int behavior_mejiro_init(const struct device *dev) {
     return 0;
 }
 
-#define MEJIRO_INST(n)                                                                       \
-    DEVICE_DT_INST_DEFINE(n,                                                                 \
-                          behavior_mejiro_init,                                              \
-                          NULL,                                                              \
-                          NULL,                                                              \
-                          NULL,                                                              \
-                          APPLICATION,                                                       \
-                          CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,                               \
-                          &behavior_mejiro_driver_api);
+#define MEJIRO_INST(n) \
+    DEVICE_DT_INST_DEFINE(n, \
+        behavior_mejiro_init, NULL, NULL, NULL, \
+        APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, \
+        &behavior_mejiro_driver_api);
 
 DT_INST_FOREACH_STATUS_OKAY(MEJIRO_INST)
-
