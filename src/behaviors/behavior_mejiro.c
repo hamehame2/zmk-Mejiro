1777397152104cbcb6654ae328e852b95ac6400f
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-21 20:38:13 +0900

    Update behavior_mejiro.c

--- src/behaviors/behavior_mejiro.c
+++ src/behaviors/behavior_mejiro.c
@@ -1,37 +1,30 @@
 #define DT_DRV_COMPAT zmk_behavior_mejiro
 
 #include <zephyr/device.h>
-#include <zephyr/logging/log.h>
 #include <drivers/behavior.h>
+#include <zephyr/logging/log.h>
+
 #include <zmk/behavior.h>
 
-LOG_MODULE_REGISTER(behavior_mejiro, CONFIG_ZMK_LOG_LEVEL);
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 static int mejiro_pressed(struct zmk_behavior_binding *binding,
                           struct zmk_behavior_binding_event event) {
-    ARG_UNUSED(binding);
-    ARG_UNUSED(event);
-    LOG_INF("MJ press");
+    LOG_INF("MEJIRO pressed pos=%d layer=%d", event.position, event.layer);
     return 0;
 }
 
 static int mejiro_released(struct zmk_behavior_binding *binding,
                            struct zmk_behavior_binding_event event) {
-    ARG_UNUSED(binding);
-    ARG_UNUSED(event);
-    LOG_INF("MJ release");
+    LOG_INF("MEJIRO released pos=%d layer=%d", event.position, event.layer);
     return 0;
 }
 
-static const struct behavior_driver_api api = {
+static const struct behavior_driver_api behavior_mejiro_driver_api = {
     .binding_pressed = mejiro_pressed,
     .binding_released = mejiro_released,
 };
 
-static int init(const struct device *dev) { ARG_UNUSED(dev); return 0; }
-
-#define MEJIRO_INST(n) \
-  DEVICE_DT_INST_DEFINE(n, init, NULL, NULL, NULL, POST_KERNEL, \
-                        CONFIG_KERNEL_INIT_PRIORITY_DEFAULT, &api);
-
-DT_INST_FOREACH_STATUS_OKAY(MEJIRO_INST)
+BEHAVIOR_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL,
+                        POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                        &behavior_mejiro_driver_api);
