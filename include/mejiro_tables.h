3c7408c9cebc4af680a5664a7ecac423c97cb651
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-25 23:30:19 +0900

    Update mejiro_tables.h

--- include/mejiro_tables.h
+++ include/mejiro_tables.h
@@ -1,28 +1,2 @@
 #pragma once
-#include <stddef.h>
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-/* Tagged struct is REQUIRED because callers use `struct mj_kv` (not a typedef). */
-struct mj_kv {
-    const char *k;
-    const char *v;
-};
-
-/* Minimal table surfaces used by mejiro_core. */
-extern const struct mj_kv mj_commands[];
-extern const size_t mj_commands_len;
-
-extern const struct mj_kv mj_users[];
-extern const size_t mj_users_len;
-
-extern const struct mj_kv mj_abstract[];
-extern const size_t mj_abstract_len;
-
-extern const struct mj_kv mj_verbs[];
-extern const size_t mj_verbs_len;
-
-#ifdef __cplusplus
-}
-#endif
+#include <mejiro/mejiro_tables.h>
