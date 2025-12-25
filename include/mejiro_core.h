7d7f7d7f06fe9e2b525da30f31f0df6688e2d4c7
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-25 23:49:08 +0900

    Update mejiro_core.h

--- include/mejiro_core.h
+++ include/mejiro_core.h
@@ -1,5 +1,2 @@
 #pragma once
-#include <stdbool.h>
-#include <stdint.h>
-#include <zmk_naginata/nglistarray.h>
-bool mejiro_try_emit_from_nginput(const NGListArray *nginput, int64_t timestamp);
+#include <mejiro/mejiro_core.h>
