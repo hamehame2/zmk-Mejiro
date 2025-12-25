9eed15183558f6c73642386a3e692b40b7530733
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-25 23:49:46 +0900

    Update mejiro_tables.h

--- include/mejiro/mejiro_tables.h
+++ include/mejiro/mejiro_tables.h
@@ -1,20 +1,14 @@
 #pragma once
-
 #include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Tagged struct: callers use `struct mj_kv` */
 struct mj_kv {
     const char *k;
     const char *v;
 };
-
-/* Tables */
-extern const struct mj_kv mj_commands[];
-extern const size_t mj_commands_len;
 
 extern const struct mj_kv mj_users[];
 extern const size_t mj_users_len;
@@ -24,6 +18,9 @@ extern const size_t mj_abstract_len;
 
 extern const struct mj_kv mj_verbs[];
 extern const size_t mj_verbs_len;
+
+extern const struct mj_kv mj_commands[];
+extern const size_t mj_commands_len;
 
 #ifdef __cplusplus
 }
