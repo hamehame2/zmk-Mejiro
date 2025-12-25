5c51d733fa42cd325f842f4528143d0c361c21df
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-25 23:01:36 +0900

    Update mejiro_core.h

--- include/mejiro/mejiro_core.h
+++ include/mejiro/mejiro_core.h
@@ -1,14 +1,28 @@
+/* -------------------------------------------------------------------------
+ * FILE: include/mejiro/mejiro_core.h
+ * ------------------------------------------------------------------------- */
 #pragma once
 
 #include <stdbool.h>
-#include <zmk_naginata/nglistarray.h>
+#include <stdint.h>
+#include <stddef.h>
+
+/* 7 keys exposed via DT binding cells (dt-bindings/zmk/mejiro.h):
+ *   MJ_H, MJ_TL, MJ_NL, MJ_TR, MJ_NR, MJ_KL, MJ_KR
+ *
+ * We treat them as bit positions 0..6.
+ */
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Called by the Naginata hook when a chord is finalized. */
-bool mejiro_try_emit_from_nginput(const NGListArray *nginput);
+/* Called from behavior driver */
+void mejiro_core_on_press(uint8_t key_id);
+void mejiro_core_on_release(uint8_t key_id);
+
+/* If you want to force flush (e.g. layer change) */
+void mejiro_core_reset(void);
 
 #ifdef __cplusplus
 }
