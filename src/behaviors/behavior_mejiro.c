88d8e62ce38becb5a5d45b94005350c87a818df3
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-26 21:47:56 +0900

    Update behavior_mejiro.c

--- src/behaviors/behavior_mejiro.c
+++ src/behaviors/behavior_mejiro.c
@@ -1,4 +1,8 @@
-// src/behaviors/behavior_mejiro.c
+/*
+ * Minimal ZMK behavior wrapper for Mejiro chords.
+ * param1: key_id (dt-bindings/zmk/mejiro.h)
+ */
+
 #define DT_DRV_COMPAT zmk_behavior_mejiro
 
 #include <zephyr/device.h>
@@ -6,45 +10,33 @@
 #include <zephyr/logging/log.h>
 
 #include <zmk/behavior.h>
-#include <zmk/event_manager.h>
-#include <zmk/events/position_state_changed.h>
+#include <zmk/keycode.h>
 
-#include <mejiro/mejiro_core.h>
-#include <mejiro/mejiro_key_ids.h>
+#include "mejiro/mejiro_core.h"
 
-LOG_MODULE_REGISTER(behavior_mejiro, CONFIG_ZMK_LOG_LEVEL);
+LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
-                                     struct zmk_behavior_binding_event event) {
-    uint32_t id = binding->param1;
-
-    if (id >= MJ_KEY_ID_MAX) {
-        LOG_WRN("mejiro: invalid key_id=%u", id);
-        return ZMK_BEHAVIOR_OPAQUE;
-    }
-
-    mejiro_core_on_press((uint8_t)id);
+static int behavior_mejiro_press(struct zmk_behavior_binding *binding,
+                                 struct zmk_behavior_binding_event event) {
+    ARG_UNUSED(event);
+    uint8_t id = (uint8_t)binding->param1;
+    mejiro_on_press(id);
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
-static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
-                                      struct zmk_behavior_binding_event event) {
-    uint32_t id = binding->param1;
-
-    if (id >= MJ_KEY_ID_MAX) {
-        LOG_WRN("mejiro: invalid key_id=%u", id);
-        return ZMK_BEHAVIOR_OPAQUE;
-    }
-
-    mejiro_core_on_release((uint8_t)id);
+static int behavior_mejiro_release(struct zmk_behavior_binding *binding,
+                                   struct zmk_behavior_binding_event event) {
+    ARG_UNUSED(event);
+    uint8_t id = (uint8_t)binding->param1;
+    mejiro_on_release(id);
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 static const struct behavior_driver_api behavior_mejiro_driver_api = {
-    .binding_pressed = on_keymap_binding_pressed,
-    .binding_released = on_keymap_binding_released,
+    .binding_pressed = behavior_mejiro_press,
+    .binding_released = behavior_mejiro_release,
 };
 
 DEVICE_DT_INST_DEFINE(0, NULL, NULL, NULL, NULL,
-                      POST_KERNEL, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
+                      APPLICATION, CONFIG_KERNEL_INIT_PRIORITY_DEFAULT,
                       &behavior_mejiro_driver_api);
