935e81fe1c32e31bc4f3929de7b111a4c92f00ab
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-26 22:59:52 +0900

    Update mejiro_send_roman.h

--- include/mejiro/mejiro_send_roman.h
+++ include/mejiro/mejiro_send_roman.h
@@ -1,19 +1,10 @@
-/* -------------------------------------------------------------------------
- * FILE: include/mejiro/mejiro_send_roman.h
- * ------------------------------------------------------------------------- */
 #pragma once
-
-#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* Execute command string like:
- *   "{#BackSpace}", "{^[]^}{#Left}{^}", "=undo", etc.
- * Return true if executed, false if unsupported/no-op.
- */
-bool mejiro_send_roman_exec(const char *cmd);
+void mejiro_send_roman_exec(const char *s);
 
 #ifdef __cplusplus
 }
