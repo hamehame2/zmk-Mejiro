79b03ed09e5492832f2ef3bbdf7dbd0e3bf38671
hamehame2 <38934289+hamehame2@users.noreply.github.com>
2025-12-26 23:08:15 +0900

    Update mejiro_send_roman.h

--- include/mejiro/mejiro_send_roman.h
+++ include/mejiro/mejiro_send_roman.h
@@ -1,10 +1,21 @@
 #pragma once
+#include <stdbool.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/**
+ * Send a roman string (ASCII) to host.
+ * This function is "fire and forget" and returns void.
+ */
 void mejiro_send_roman_exec(const char *s);
+
+/**
+ * Execute a simple command string if you are using a command-like layer.
+ * Returns true if the command was recognized and handled.
+ */
+bool mejiro_send_roman_exec_cmd(const char *cmd);
 
 #ifdef __cplusplus
 }
